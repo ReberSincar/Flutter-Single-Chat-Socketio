ba8506c35ca91b6c0f0d7e0a2d5909002c07f548
rebersincar <rebersincar07@gmail.com>
2021-08-09 17:42:00 +0300

    Image message dialog added

--- lib/widgets/my_message_container.dart
+++ lib/widgets/my_message_container.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_private_chat/models/message.dart';
+import 'package:get/get.dart';
 
 class MyMessageContainer extends StatelessWidget {
   const MyMessageContainer({
@@ -14,30 +15,39 @@ class MyMessageContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topRight,
-      child: Container(
-        margin: EdgeInsets.only(top: 10, right: 10),
-        padding: EdgeInsets.all(message.messageType == 0 ? 10 : 4),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.teal,
+      child: InkWell(
+        onTap: message.messageType == 0
+            ? null
+            : () {
+                Get.dialog(Dialog(
+                  child: Image.memory(message.image!),
+                ));
+              },
+        child: Container(
+          margin: EdgeInsets.only(top: 10, right: 10),
+          padding: EdgeInsets.all(message.messageType == 0 ? 10 : 4),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.teal,
+          ),
+          child: message.messageType == 0
+              ? Text(
+                  message.message!,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                )
+              : ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.memory(
+                    message.image!,
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
         ),
-        child: message.messageType == 0
-            ? Text(
-                message.message!,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              )
-            : ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.memory(
-                  message.image!,
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.cover,
-                ),
-              ),
       ),
     );
   }
--- lib/widgets/your_message_container.dart
+++ lib/widgets/your_message_container.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_private_chat/models/message.dart';
+import 'package:get/get.dart';
 
 class YourMessageContainer extends StatelessWidget {
   const YourMessageContainer({
@@ -14,30 +15,39 @@ class YourMessageContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Align(
       alignment: Alignment.topLeft,
-      child: Container(
-        margin: EdgeInsets.only(top: 10, left: 10),
-        padding: EdgeInsets.all(message.messageType == 0 ? 10 : 4),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: Colors.white,
+      child: InkWell(
+        onTap: message.messageType == 0
+            ? null
+            : () {
+                Get.dialog(Dialog(
+                  child: Image.memory(message.image!),
+                ));
+              },
+        child: Container(
+          margin: EdgeInsets.only(top: 10, left: 10),
+          padding: EdgeInsets.all(message.messageType == 0 ? 10 : 4),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.white,
+          ),
+          child: message.messageType == 0
+              ? Text(
+                  message.message!,
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontSize: 16,
+                  ),
+                )
+              : ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.memory(
+                    message.image!,
+                    width: 200,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
+                ),
         ),
-        child: message.messageType == 0
-            ? Text(
-                message.message!,
-                style: TextStyle(
-                  color: Colors.teal,
-                  fontSize: 16,
-                ),
-              )
-            : ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.memory(
-                  message.image!,
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.cover,
-                ),
-              ),
       ),
     );
   }
